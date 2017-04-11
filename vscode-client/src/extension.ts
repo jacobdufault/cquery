498375e9c9027844726f1e6d8a1aaaef93bf22d8
Jacob Dufault <jacobdufault@gmail.com>
2017-04-11 01:43:35 -0700

    Add "Base" code lens which jumps the editor to the base method definition.

--- vscode-client/src/extension.ts
+++ vscode-client/src/extension.ts
@@ -6,7 +6,7 @@
 
 import * as path from 'path';
 
-import { commands, window, workspace, Disposable, ExtensionContext, Location, Position, Uri, Range } from 'vscode';
+import { commands, window, workspace, Disposable, ExtensionContext, Location, Position, TextEditorRevealType, Uri, Range, Selection } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, RevealOutputChannelOn, InitializationFailedHandler, ErrorHandler, ErrorAction, CloseAction } from 'vscode-languageclient';
 import { Message } from 'vscode-jsonrpc';
 
@@ -38,6 +38,52 @@ class Foo implements ErrorHandler {
 export function activate(context: ExtensionContext) {
 	console.log('Running');
 
+	// commands.getCommands(false).then((cmds: string[]) => {
+	// 	console.log(cmds.join(' '));
+	// });
+
+	commands.registerCommand('superindex.goto', (uri, position, locations) => {
+		function parsePosition(p): Position {
+			return new Position(p.line, p.character);
+		}
+		function parseRange(r): Range {
+			return new Range(parsePosition(r.start), parsePosition(r.end));
+		}
+		function parseLocation(l): Location {
+			return new Location(parseUri(l.uri), parseRange(l.range));
+		}
+		function parseUri(u): Uri {
+			return Uri.parse(u);
+		}
+
+		let parsedPosition = parsePosition(position);
+		// let parsedUri = parseUri(uri + `#${position.line},${position.character}`);
+		// let parsedUri = parseUri(uri + `#L${position.line}`);
+		let parsedUri = parseUri(uri);
+
+    console.error('setting selection to ' + parsedPosition.line + ',' + parsedPosition.character);
+
+		// const {scheme, path, query, fragment} = parsedUri;
+		// console.log(`openTextDocument parsedUri=${parsedUri} scheme=${scheme} path=${path} query=${query} fragment=${fragment}`);
+		workspace.openTextDocument(parsedUri).then(d => {
+			console.log('got ' + d);
+
+			if (!d) {
+				window.activeTextEditor.revealRange(new Range(parsedPosition, parsedPosition), TextEditorRevealType.InCenter);
+				window.activeTextEditor.selection = new Selection(parsedPosition, parsedPosition);
+			}
+			else {
+				window.showTextDocument(d).then(e => {
+					e.revealRange(new Range(parsedPosition, parsedPosition), TextEditorRevealType.InCenter);
+					e.selection = new Selection(parsedPosition, parsedPosition);
+				})
+			}
+		})
+
+		//let parsedLocation = parseLocation(locations[0]);
+		//commands.executeCommand(
+		//	'editor.action.goToTypeDefinition', parsedUri, parsedLocation);
+	});
 
 	// Proxy editor.action.showReferences so we can deserialize arguments into
 	// the correct vscode types.
@@ -70,7 +116,7 @@ export function activate(context: ExtensionContext) {
 
 	let serverOptions: ServerOptions = {
 		command: 'indexer.exe', args: ['--language-server'], options: {
-			cwd: 'C:\\Users\\jacob\\Desktop\\superindex\\indexer\\x64\\Release',
+			cwd: 'C:\\Users\\jacob\\Desktop\\superindex\\indexer\\x64\\Debug',
 			// stdio: 'ascii'
     	//stdio?: string | string[];
 			//env?: any;
