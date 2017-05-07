4b39e273a409b415d7a93b5f23118550fb5041ab
Jacob Dufault <jacobdufault@gmail.com>
2017-05-07 12:09:15 -0700

    Fix issue #1, cleanup vscode-client code.

--- vscode-client/src/extension.ts
+++ vscode-client/src/extension.ts
@@ -1,7 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import * as path from 'path';
@@ -10,15 +6,7 @@ import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, RevealOutputChannelOn, InitializationFailedHandler, ErrorHandler, ErrorAction, CloseAction } from 'vscode-languageclient';
 import { Message } from 'vscode-jsonrpc';
 
-class Foo implements ErrorHandler {
-    /**
-     * An error has occurred while writing or reading from the connection.
-     *
-     * @param error - the error received
-     * @param message - the message to be delivered to the server if know.
-     * @param count - a count indicating how often an error is received. Will
-     *  be reset if a message got successfully send or received.
-     */
+class MyErrorHandler implements ErrorHandler {
     error(error: Error, message: Message, count: number): ErrorAction {
       console.log('!!!! error');
       console.log(error);
@@ -26,11 +14,7 @@ class Foo implements ErrorHandler {
       console.log(count);
       return ErrorAction.Continue;
     }
-    /**
-     * The connection to the server got closed.
-     */
     closed(): CloseAction	{
-      console.log('!!!! closed');
       return CloseAction.DoNotRestart;
     }
 }
@@ -56,52 +40,9 @@ function parseUri(u): vscode.Uri {
 
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Running');
-
-  // commands.getCommands(false).then((cmds: string[]) => {
-  // 	console.log(cmds.join(' '));
-  // });
-
-  vscode.commands.registerCommand('cquery.goto', (uri, position, locations) => {
-    let parsedPosition = parsePosition(position);
-    // let parsedUri = parseUri(uri + `#${position.line},${position.character}`);
-    // let parsedUri = parseUri(uri + `#L${position.line}`);
-    let parsedUri = parseUri(uri);
-
-    console.error('setting selection to ' + parsedPosition.line + ',' + parsedPosition.character);
-
-    // const {scheme, path, query, fragment} = parsedUri;
-    // console.log(`openTextDocument parsedUri=${parsedUri} scheme=${scheme} path=${path} query=${query} fragment=${fragment}`);
-    vscode.workspace.openTextDocument(parsedUri).then(d => {
-      console.log('got ' + d);
-
-      if (!d) {
-        vscode.window.activeTextEditor.revealRange(new vscode.Range(parsedPosition, parsedPosition), vscode.TextEditorRevealType.InCenter);
-        vscode.window.activeTextEditor.selection = new vscode.Selection(parsedPosition, parsedPosition);
-      }
-      else {
-        vscode.window.showTextDocument(d).then(e => {
-          e.revealRange(new vscode.Range(parsedPosition, parsedPosition), vscode.TextEditorRevealType.InCenter);
-          e.selection = new vscode.Selection(parsedPosition, parsedPosition);
-        })
-      }
-    })
-
-    //let parsedLocation = parseLocation(locations[0]);
-    //commands.executeCommand(
-    //	'editor.action.goToTypeDefinition', parsedUri, parsedLocation);
-  });
-
-  // Proxy editor.action.showReferences so we can deserialize arguments into
-  // the correct vscode types.
-  vscode.commands.registerCommand('cquery.showReferences', (uri, position, locations) => {
-    let parsedUri = parseUri(uri);
-    let parsedPosition = parsePosition(position);
-    let parsedLocations = parseLocations(locations);
-    vscode.commands.executeCommand(
-      'editor.action.showReferences',
-      parsedUri, parsedPosition, parsedLocations);
-  });
+  /////////////////////////////////////
+  // Setup configuration, start server.
+  /////////////////////////////////////
 
   let config = vscode.workspace.getConfiguration('cquery');
   let clientConfig = {
@@ -117,6 +58,13 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   if (!clientConfig.cacheDirectory) {
+    if (!context.storagePath) {
+      vscode.window.showErrorMessage(
+          'Could not auto-discover cache directory. Please use "Open Folder" ' +
+          'or specify it in the |cquery.cacheDirectory| setting.');
+      return;
+    }
+
     console.log('Updating cache directory')
     clientConfig.cacheDirectory = context.storagePath.replace(/\\/g, '/') + "/cquerycache/"
     config.update('cacheDirectory',
@@ -132,13 +80,9 @@ export function activate(context: vscode.ExtensionContext) {
   let serverOptions: ServerOptions = {
       command: 'indexer.exe', args: ['--language-server'], options: {
       cwd: 'C:/Users/jacob/Desktop/superindex/indexer/x64/Release',
-      // stdio: 'ascii'
-      //stdio?: string | string[];
       // env: {
       //   'MALLOC_CHECK_': '2'
       // }
-      //env?: any;
-      //detached?: boolean
     }
   }
 
@@ -152,36 +96,23 @@ export function activate(context: vscode.ExtensionContext) {
     diagnosticCollectionName: 'cquery',
     outputChannelName: 'cquery',
     revealOutputChannelOn: RevealOutputChannelOn.Error,
-    /**
-     * The encoding use to read stdout and stderr. Defaults
-     * to 'utf8' if ommitted.
-     */
-    stdioEncoding: 'ascii',
     initializationOptions: clientConfig,
-    // initializationOptions?: any | (() => any);
     initializationFailedHandler: (e) => {
       console.log(e);
       return false;
     },
-    errorHandler: new Foo()
-    // uriConverters?: {
-        // code2Protocol: c2p.URIConverter;
-        // protocol2Code: p2c.URIConverter;
-    // }
+    errorHandler: new MyErrorHandler()
   }
 
   // Create the language client and start the client.
   let languageClient = new LanguageClient('cquery', 'cquery', serverOptions, clientOptions);
-  languageClient.onReady().then(() => {
-    console.log('!! onReady');
-    // languageClient.onNotification('window/showMessage', (m) => {
-      // console.log('!!!! onNotification')
-      // console.log(m);
-    // });
-  });
-  console.log("starting");
   let disposable = languageClient.start();
   context.subscriptions.push(disposable);
+
+
+  /////////////////////
+  // Register commands.
+  /////////////////////
 
   vscode.commands.registerCommand('cquery.freshenIndex', () => {
     languageClient.sendNotification('$cquery/freshenIndex');
@@ -195,7 +126,6 @@ export function activate(context: vscode.ExtensionContext) {
         textDocument: { uri: uri.toString(), },
         position: position
       }).then((locations: Array<object>) => {
-        // console.log(`${methodName} handler got ${locations.length} responses`);
         if (autoGotoIfSingle && locations.length == 1) {
           let location = parseLocation(locations[0])
           vscode.commands.executeCommand(
@@ -213,4 +143,33 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand('cquery.callers', makeRefHandler('$cquery/callers'));
   vscode.commands.registerCommand('cquery.base', makeRefHandler('$cquery/base', true));
   vscode.commands.registerCommand('cquery.derived', makeRefHandler('$cquery/derived'));
+
+  // The language client does not correctly deserialize arguments, so we have a
+  // wrapper command that does it for us.
+  vscode.commands.registerCommand('cquery.showReferences', (uri, position, locations) => {
+    vscode.commands.executeCommand(
+      'editor.action.showReferences',
+      parseUri(uri), parsePosition(position), parseLocations(locations));
+  });
+
+
+  vscode.commands.registerCommand('cquery.goto', (uri, position, locations) => {
+    let parsedPosition = parsePosition(position);
+    let parsedUri = parseUri(uri);
+
+    vscode.workspace.openTextDocument(parsedUri).then(d => {
+      if (!d) {
+        vscode.window.activeTextEditor.revealRange(new vscode.Range(parsedPosition, parsedPosition), vscode.TextEditorRevealType.InCenter);
+        vscode.window.activeTextEditor.selection = new vscode.Selection(parsedPosition, parsedPosition);
+      }
+      else {
+        vscode.window.showTextDocument(d).then(e => {
+          e.revealRange(new vscode.Range(parsedPosition, parsedPosition), vscode.TextEditorRevealType.InCenter);
+          e.selection = new vscode.Selection(parsedPosition, parsedPosition);
+        })
+      }
+    })
+  });
+
+
 }
