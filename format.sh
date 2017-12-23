cfe9348eec376280ada91b5c3d9bd32d16c6de37
Jacob Dufault <jacobdufault@gmail.com>
2017-12-23 08:01:43 -0800

    Format using clang format 4.0.0

--- format.sh
+++ format.sh
@@ -9,9 +9,12 @@
 #   ./format.sh
 #
 
+CLANG_FORMAT=./build/clang+llvm-4.0.0-x86_64-apple-darwin/bin/clang-format
+echo "Using CLANG_FORMAT=$CLANG_FORMAT"
+
 FILES=$(find src -name '*.cc' -o -name '*.h' | grep -v '/\.')
 for FILE in $FILES; do
   echo "Formatting $FILE"
-  clang-format -style=Chromium -i $FILE
+  $CLANG_FORMAT -style=Chromium -i $FILE
 done
 
