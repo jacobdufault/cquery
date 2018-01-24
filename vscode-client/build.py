b245983a2ca654d65c857c43eb094ce0b9ff4e21
Jacob Dufault <jacobdufault@gmail.com>
2018-01-24 10:07:32 -0800

    Remove vscode-client

--- vscode-client/build.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-import sys
-
-if __name__ == "__main__":
-    vsce = 'vsce.cmd' if sys.platform == 'win32' else 'vsce'
-    sys.exit(subprocess.call([vsce, 'package', '-o', '../vscode-extension.vsix']))
