bd87621dac92dd743a36ae3a81fb9237878a3686
Fangrui Song <i@maskray.me>
2018-02-01 12:21:58 -0800

    Handle int8_t int16_t uint8_t uint16_t enum constants
    Clean up clang_complete

--- index_tests/enums/enum_class_decl.cc
+++ index_tests/enums/enum_class_decl.cc
@@ -1,4 +1,5 @@
-enum class Foo {
+typedef unsigned char uint8_t;
+enum class Foo : uint8_t {
   A,
   B = 20
 };
@@ -10,12 +11,28 @@ OUTPUT:
   "skipped_by_preprocessor": [],
   "types": [{
       "id": 0,
+      "usr": 2010430204259339553,
+      "detailed_name": "uint8_t",
+      "short_name": "uint8_t",
+      "kind": 11,
+      "hover": "typedef unsigned char uint8_t",
+      "definition_spelling": "1:23-1:30",
+      "definition_extent": "1:1-1:30",
+      "parents": [],
+      "derived": [],
+      "types": [],
+      "funcs": [],
+      "vars": [],
+      "instances": [],
+      "uses": ["1:23-1:30", "2:12-2:15"]
+    }, {
+      "id": 1,
       "usr": 16985894625255407295,
       "detailed_name": "Foo",
       "short_name": "Foo",
       "kind": 5,
-      "definition_spelling": "1:12-1:15",
-      "definition_extent": "1:1-4:2",
+      "definition_spelling": "2:12-2:15",
+      "definition_extent": "2:1-5:2",
       "parents": [],
       "derived": [],
       "types": [],
@@ -32,11 +49,11 @@ OUTPUT:
       "short_name": "A",
       "hover": "Foo::A = 0",
       "declarations": [],
-      "definition_spelling": "2:3-2:4",
-      "definition_extent": "2:3-2:4",
-      "variable_type": 0,
+      "definition_spelling": "3:3-3:4",
+      "definition_extent": "3:3-3:4",
+      "variable_type": 1,
       "uses": [],
-      "parent_id": 0,
+      "parent_id": 1,
       "parent_kind": 2,
       "kind": 15,
       "storage": 0
@@ -47,11 +64,11 @@ OUTPUT:
       "short_name": "B",
       "hover": "Foo::B = 20",
       "declarations": [],
-      "definition_spelling": "3:3-3:4",
-      "definition_extent": "3:3-3:9",
-      "variable_type": 0,
+      "definition_spelling": "4:3-4:4",
+      "definition_extent": "4:3-4:9",
+      "variable_type": 1,
       "uses": [],
-      "parent_id": 0,
+      "parent_id": 1,
       "parent_kind": 2,
       "kind": 15,
       "storage": 0
