8f4cd58103fef70de6ede22468c90b9da57508e5
Chao Shen <chao.shen@uisee.com>
2018-02-06 09:53:50 +0800

    Fix compilation.

--- third_party/string_view.hpp
+++ third_party/string_view.hpp
@@ -30,7 +30,7 @@
 #endif
 
 #if defined(__has_include) && !defined(STX_NO_STD_STRING_VIEW)
-#if __has_include(<string_view>)
+#if __has_include(<string_view>) && ((__cplusplus > 201402) || defined(_LIBCPP_VERSION))
         #include <string_view>
         #define STX_HAVE_STD_STRING_VIEW 1
         namespace STX_NAMESPACE_NAME {
