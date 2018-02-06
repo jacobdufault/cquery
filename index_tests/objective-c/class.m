d44e6ccfa80daea6cf215e2a462295424bd2edb2
Chao Shen <chao.shen@uisee.com>
2018-02-06 10:05:16 +0800

    Try to fix travis.

--- index_tests/objective-c/class.m
+++ index_tests/objective-c/class.m
@@ -24,8 +24,8 @@ OUTPUT:
   "types": [{
       "id": 0,
       "usr": 11832280568361305387,
-      "short_name": "AClass",
       "detailed_name": "AClass",
+      "short_name": "AClass",
       "kind": 7,
       "definition_spelling": "7:17-7:23",
       "definition_extent": "7:1-10:2",
@@ -34,16 +34,29 @@ OUTPUT:
       "types": [],
       "funcs": [],
       "vars": [],
-      "instances": [],
-      "uses": ["1:12-1:18", "7:17-7:23", "14:3-14:9", "14:23-14:29"]
+      "instances": [2],
+      "uses": ["14:3-14:9", "14:23-14:29"]
+    }, {
+      "id": 1,
+      "usr": 17,
+      "detailed_name": "",
+      "short_name": "",
+      "kind": 0,
+      "parents": [],
+      "derived": [],
+      "types": [],
+      "funcs": [],
+      "vars": [],
+      "instances": [0, 1],
+      "uses": []
     }],
   "funcs": [{
       "id": 0,
-      "is_operator": false,
       "usr": 12775970426728664910,
-      "short_name": "test",
       "detailed_name": "AClass::test",
+      "short_name": "test",
       "kind": 17,
+      "storage": 0,
       "declarations": [{
           "spelling": "2:11-2:15",
           "extent": "2:3-2:16",
@@ -59,11 +72,11 @@ OUTPUT:
       "callees": []
     }, {
       "id": 1,
-      "is_operator": false,
       "usr": 4096877434426330804,
-      "short_name": "anInstanceMethod",
       "detailed_name": "AClass::anInstanceMethod",
+      "short_name": "anInstanceMethod",
       "kind": 16,
+      "storage": 0,
       "declarations": [{
           "spelling": "3:11-3:27",
           "extent": "3:3-3:28",
@@ -79,18 +92,17 @@ OUTPUT:
       "callees": []
     }, {
       "id": 2,
-      "is_operator": false,
       "usr": 12774569141855220778,
-      "short_name": "aProp",
       "detailed_name": "AClass::aProp",
+      "short_name": "aProp",
       "kind": 16,
+      "storage": 0,
       "declarations": [{
           "spelling": "0:0-0:0",
           "extent": "4:29-4:34",
           "content": "aProp",
           "param_spellings": []
         }],
-      "definition_spelling": "0:0-0:0",
       "definition_extent": "4:29-4:34",
       "base": [],
       "derived": [],
@@ -99,18 +111,17 @@ OUTPUT:
       "callees": []
     }, {
       "id": 3,
-      "is_operator": false,
       "usr": 17992064398538597892,
-      "short_name": "setAProp:",
       "detailed_name": "AClass::setAProp:",
+      "short_name": "setAProp:",
       "kind": 16,
+      "storage": 0,
       "declarations": [{
           "spelling": "0:0-0:0",
           "extent": "4:29-4:34",
           "content": "aProp",
           "param_spellings": ["4:29-4:34"]
         }],
-      "definition_spelling": "0:0-0:0",
       "definition_extent": "4:29-4:34",
       "base": [],
       "derived": [],
@@ -119,11 +130,11 @@ OUTPUT:
       "callees": []
     }, {
       "id": 4,
-      "is_operator": false,
       "usr": 7033269674615638282,
-      "short_name": "main",
       "detailed_name": "int main()",
+      "short_name": "main",
       "kind": 12,
+      "storage": 1,
       "declarations": [],
       "definition_spelling": "12:5-12:9",
       "definition_extent": "12:1-17:2",
@@ -136,30 +147,42 @@ OUTPUT:
   "vars": [{
       "id": 0,
       "usr": 14842397373703114213,
-      "short_name": "aProp",
       "detailed_name": "int AClass::aProp",
-      "declaration": "4:29-4:34",
+      "short_name": "aProp",
+      "declarations": ["4:29-4:34"],
+      "variable_type": 1,
+      "uses": ["16:12-16:17"],
+      "parent_kind": 0,
       "kind": 19,
-      "uses": ["4:29-4:34", "16:12-16:17"]
+      "storage": 0
     }, {
       "id": 1,
       "usr": 17112602610366149042,
-      "short_name": "_aProp",
       "detailed_name": "int AClass::_aProp",
+      "short_name": "_aProp",
+      "declarations": [],
       "definition_spelling": "4:29-4:34",
       "definition_extent": "4:29-4:34",
+      "variable_type": 1,
+      "uses": [],
+      "parent_kind": 0,
       "kind": 14,
-      "uses": ["4:29-4:34"]
+      "storage": 0
     }, {
       "id": 2,
       "usr": 6849095699869081177,
-      "short_name": "instance",
       "detailed_name": "AClass *instance",
+      "short_name": "instance",
       "hover": "AClass *instance = [AClass init]",
+      "declarations": [],
       "definition_spelling": "14:11-14:19",
       "definition_extent": "14:3-14:35",
+      "variable_type": 0,
+      "uses": ["15:4-15:12", "16:3-16:11"],
+      "parent_id": 4,
+      "parent_kind": 3,
       "kind": 13,
-      "uses": ["14:11-14:19", "15:4-15:12", "16:3-16:11"]
+      "storage": 1
     }]
 }
 */
