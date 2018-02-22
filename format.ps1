bf20c6d03ed406d70110e9f22b648371982b4019
Jacob Dufault <jacobdufault@gmail.com>
2018-02-21 23:34:32 -0800

    Format code

--- format.ps1
+++ format.ps1
@@ -8,5 +8,5 @@ $files=Get-ChildItem -Recurse src -Include *.h,*.cc |
     ? { $_.FullName -inotmatch '.vscode' }
 foreach ($file in $files) {
   Write-Host "Formatting" $file.FullName
-  clang-format.exe -style=Chromium -i $file.FullName
+  .\build\LLVM-5.0.1-win64\bin\clang-format.exe -style=Chromium -i $file.FullName
 }
