40a2f8ace2234ec6a2d6d2af7c263ddde7b113f1
ddovod <ddovod@gmail.com>
2018-09-01 21:46:04 +0300

    adding virtual dtor to CompletionMatcher

--- src/completion_matcher.h
+++ src/completion_matcher.h
@@ -4,7 +4,7 @@
 
 class CompletionMatcher {
  public:
-  // virtual ~CompletionMatcher() = default; // don't know why but it crashed if I uncomment this like, investigating
+  virtual ~CompletionMatcher() = default;
   virtual int Match(std::string_view text) = 0;
   virtual int MinScore() const = 0;
 };
