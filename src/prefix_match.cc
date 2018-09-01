4d53c1d06a4fd936a59906cd9eeaf1411086eec6
ddovod <ddovod@gmail.com>
2018-09-01 21:48:02 +0300

    using string utilities from utils.h

--- src/prefix_match.cc
+++ src/prefix_match.cc
@@ -1,24 +1,7 @@
 #include "prefix_match.h"
 
+#include "utils.h"
 #include <algorithm>
-
-namespace {
-    bool StartsWith(std::string_view text, std::string_view prefix) {
-      return text.find(prefix) == 0;
-    }
-
-    bool StartsWithIgnoreCase(std::string_view text, std::string_view prefix) {
-      auto min_length = std::min(text.size(), prefix.size());
-      if (min_length == 0) {
-        return false;
-      }
-      return std::equal(
-          text.begin(), text.begin() + min_length,
-          prefix.begin(), prefix.begin() + min_length,
-          [](char ch1, char ch2) { return std::toupper(ch1) == std::toupper(ch2); }
-          );
-    }
-}
 
 PrefixMatcher::PrefixMatcher(std::string_view pattern, bool case_sensitive) {
   original_pattern = pattern;
