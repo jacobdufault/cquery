5fde29853791047103c2ce9b2492c25f59a7c51b
gabrieldeml <gabrielddeml@gmail.com>
2020-07-03 15:25:34 -0400

    cquery would not compile without adding #include<string>

--- src/options.h
+++ src/options.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <unordered_map>
-
+#include <string>
 std::unordered_map<std::string, std::string> ParseOptions(int argc,
                                                           char** argv);
 
